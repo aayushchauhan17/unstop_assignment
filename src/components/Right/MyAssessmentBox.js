5460d5073492352baa15c9572180e50813cdbad1
aayushchauhan17 <aayushchauhan176@gmail.com>
2023-08-28 17:02:27 +0530

    desktop complete

--- src/components/Right/MyAssessmentBox.js
+++ src/components/Right/MyAssessmentBox.js
@@ -4,14 +4,18 @@ import BagImg from "../../assets/images/bagImg.svg";
 import Dots from "../../assets/images/dots.svg";
 import Cal from "../../assets/images/calendar_today.svg";
 import Link from "../../assets/images/link.svg";
+import Add from "../../assets/images/add.svg";
+import Cross from "../../assets/images/cross.svg";
+
 
 function MyAssessmentBox({ add = false }) {
+  const [showModel, setShowModel] = React.useState(false);
   return (
     <>
       <div className="box-conatainer">
         {add ? (
           <div className="new-ass">
-            <h1>+</h1>
+            <img onClick={()=>{setShowModel(true)}} className="add-img" src={Add} alt="add" />
             <h2>New Assessment</h2>
             <p>
               From here you can add questions of multiple types like MCQs,
@@ -55,6 +59,81 @@ function MyAssessmentBox({ add = false }) {
           </div>
         )}
       </div>
+      {showModel && <>
+        <div className="model-container">
+          <div className="model-body">
+            <div className="model-header">
+              <h1>
+                Create New Asignment
+              </h1>
+              <img onClick={()=>{
+                setShowModel(false);
+              }} width={32} height={30} src={Cross} alt="cross" />
+            </div>
+            <div className="model-body">
+              <div className="model-input-box">
+                <p>Name of assessment</p>
+                <input type="text" placeholder="Type Here" />
+              </div>
+              <div className="model-input-box">
+                <p>Purpose of the test isnt</p>
+                <select>
+                  <option>Select</option>
+                  <option>Option2</option>
+                  <option>Option3</option>
+                </select>
+              </div>
+              <div className="model-input-box">
+                <p>Purpose of the test isnt</p>
+                <select>
+                  <option>Select</option>
+                  <option>Option2</option>
+                </select>
+              </div>
+
+              <div className="model-input-box">
+                <p>Skills</p>
+                <div className="model-skills">
+                  <span className="model-select-skill">
+                    <p>UI/UX and Design</p>
+                    <img width={18} height={18} src={Cross} alt="cross" />
+                  </span>
+
+                  <span className="model-select-skill">
+                    <p>No of Question</p>
+                    <img width={18} height={18} src={Cross} alt="cross" />
+                  </span>
+
+                  <span className="model-select-skill">
+                    <p>Web Development</p>
+                    <img width={18} height={18} src={Cross} alt="cross" />
+                  </span>
+
+                  <span className="model-select-skill">
+                    <p>UI/UX and Design</p>
+                    <img width={18} height={18} src={Cross} alt="cross" />
+                  </span>
+
+                  <span className="model-select-skill">
+                    <p>Web Development</p>
+                    <img width={18} height={18} src={Cross} alt="cross" />
+                  </span>
+                </div>
+                <input className="model-skill-input" type="text" placeholder="Type Here" />
+              </div>
+
+              <div className="model-input-box">
+                <p>Duration of assessment</p>
+                <input type="text"  placeholder="HH:MM:SS" />
+              </div>
+            </div>
+
+            <div className="model-save-button">
+              <button>Save</button>
+            </div>
+          </div>
+        </div>
+      </>}
     </>
   );
 }
